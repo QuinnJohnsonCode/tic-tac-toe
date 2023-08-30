1cfcf9198a6ce1a9e2a8824a9bb32787670f01f8
Quinn Johnson <63220163+QuinnJohnsonCode@users.noreply.github.com>
2023-08-29 22:25:02 -0400

    Add PlayerFactory and create two players

--- js/game.js
+++ js/game.js
@@ -1,9 +1,9 @@
 let gameBoard = (function() {
-    // Private Functions
+    // Private
     const board = Array(9).fill("-"); // Fill array with 9 "empty" values
 
     return {
-        // Public Functions
+        // Public
         getBoardRepresentation: function() {
             let boardRepresentation = "";
             for (let i = 0; i < board.length; ++i) {
@@ -15,4 +15,30 @@ let gameBoard = (function() {
     };
 })();
 
-console.log(gameBoard.getBoardRepresentation());
+let PlayerFactory = marker => {
+    // Store player wins/losses/draws
+    let wins = 0;
+    let losses = 0;
+    let draws = 0;
+
+    const win =  () => ++wins;
+    const lose = () => ++losses;
+    const draw = () => ++draws;
+    const getMarker = () => marker;
+    
+    return { getMarker, win, lose, draw};
+};
+
+// game is a Module controlling gameflow and state
+let game = (function(gameBoard) {
+    // Private
+    const xPlayer = PlayerFactory("X");
+    const oPlayer = PlayerFactory("O");
+
+    console.log(xPlayer.win());
+
+    return {
+        // Public
+    };
+})(gameBoard);
+
